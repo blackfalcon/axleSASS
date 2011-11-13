f40c9c5a8857f13c38e7b15bbefc814312806d25
Dale Sande <dale.sande@gmail.com>
2011-11-12 21:42:58 -0800

    installing compass.app config and doing some house cleaning

--- index.php
+++ index.php
@@ -1,57 +1,68 @@
 <!DOCTYPE html>
 <html class="no-js">
 <head>
-	<meta charset="UTF-8">	
-	<title>Unmatched Style CSSOFF!!</title>
-	<meta name="description" content="Unmatched Style contest to show how awesome we really are">
-	<meta name="keywords" content="awesome, css, html5, css3, killer site, crazy contest">
-	<meta name="author" content="Dale Sande">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-	<!--  Mobile/Handheld Viewport -->
-	<!-- <meta name="viewport" content="initial-scale=1, maximum-scale=1" /> -->
+	<title>This is awesome is that this is!</title>
+	
+	<meta http-equiv="Content-Language" content="en-us" />
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
 	
 	<!-- Place favicon.ico and apple-touch-icon.png in the root of your domain and delete these references -->
 	<link rel="shortcut icon" href="/public/images/favicon.ico">
 	<link rel="apple-touch-icon" href="/public/images/apple-touch-icon.png">
 	
 	<!-- CSS : implied media="screen" -->
-	<link rel="stylesheet" href="public/styles/CSS/core_style.css" />
+	<link rel="stylesheet" href="/public/styles/CSS/core_style.css" />
 	
-	<script src="/public/javascripts/jquery.min.js"></script>
-	
-	<!-- legacy browser support ... IE must die -->
-	<script src="/public/javascripts/modernizr-1.7.min.js"></script> <!-- we know the awesome -->
-	<!--[if (gte IE 6)&(lte IE 9)]>
-		<script type="text/javascript" src="/public/javascripts/selectivizr-min.js"></script>
-		<script src="/public/javascripts/flexie.min.js"></script> <!-- pollyfill for IE flexbox support -->
-	<![endif]-->
-	
-	<script src="/public/javascripts/css_browser_selector.js"></script> <!-- basic platform and browser support -->
-	
-	<!-- application specific scripts -->
+	<script src="http://code.jquery.com/ui/1.8.16/jquery-ui.min.js"></script>
+	<script src="http://www.modernizr.com/downloads/modernizr-2.0.6.js"></script>
+	<script src="/public/javascripts/selectivizr-min.js"></script>
+	<script src="/public/javascripts/css_browser_selector.js"></script>
 	<script src="/public/javascripts/application.js"></script>	
+
 </head>
 
 <body>
-	<?php include 'includes/site_nav.php'?>
-	<section class="page_container">
-		<?php include 'includes/site_header.php'?>
-	</section>
-	<section class="page_container">
-		<div class="box_shadow"></div>
-		<a name="obstacles"></a>
-		<?php include 'includes/obstacles.php'?>
-	</section>
-	<section class="page_container">
-		<div class="box_shadow"></div>
-		<a name="prizes"></a>
-		<?php include 'includes/prizes.php'?>
-	</section>
-	<section class="page_container">
-		<div class="box_shadow"></div>
-		<a name="contest"></a>
-		<?php include 'includes/contest_form.php'?>
-		<?php include 'includes/site_footer.php'?>
+	<section class="site_container">
+		
+		<header>
+			<h1>site header</h1>
+		</header>
+		
+		<section class="left">
+			left nav?
+		</section>
+		
+		<section class="right">
+			
+			<header>
+				<h1>page header</h1>
+			</header>
+			
+			<article>
+				
+				<header>
+					<h1>article header</h1>
+				</header>
+				
+				<aside>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</aside>
+				
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				
+				<footer>article footer</footer>
+			
+			</article>
+		
+		</section>
+		
+		<footer>site footer</footer>
+	
 	</section>
 </body>
 </html>
